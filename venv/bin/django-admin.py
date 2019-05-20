57efb5ec7b0ba5dd8b43962e60687691afd435a6
aditya <adityaprayaga@gmail.com>
2019-05-21 00:34:42 +0530

    day-1 : venv + django setup

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/aditya-is-seeing-you/Downloads/Git Repos /Temporary/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
